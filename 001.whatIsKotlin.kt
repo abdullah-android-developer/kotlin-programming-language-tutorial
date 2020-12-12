2ca2ac8714b2273a0203a0ebcd45e81161087fa9
Abdullah Sajjad <75803347+abdullah-android-developer@users.noreply.github.com>
2020-12-12 11:54:11 +0500

    Add files via upload

---
+++ 001.whatIsKotlin.kt
@@ -0,0 +1,9 @@
+// What is Kotlin?
+
+/*
+*   - Kotlin is a General Purpose, Statically Typed, and Cross Platform Programming Language.
+*   - Kotlin runs on JVM (Java Virtual Machine), which means it is fully interoperable with Java.
+*   - It is less verbose.
+*   - It is used in Android App Development and Web Development.
+*   - It is easy to learn.
+* */
