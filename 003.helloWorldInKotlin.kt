9f3babc85a7354c63528c235d4a4f52208226fc5
Abdullah Sajjad <75803347+abdullah-android-developer@users.noreply.github.com>
2020-12-13 16:22:06 +0500

    Add files via upload

---
+++ 003.helloWorldInKotlin.kt
@@ -0,0 +1,17 @@
+// Code:-
+
+// Writing Hello World Code In Kotlin:-
+
+// 1. fun is the keyword to make functions.
+// 2. main() is the function from which our code starts executing.
+fun main() {
+	// 3. print() is the function which print and give us a output without a new line at end.
+	print("hello world, ")
+	// 4. println() is the function which print and give us a output with a new line at a end.
+	println("Abdullah")
+	print("Sajjad")
+}
+/* 5. Output:-
+			hello world, Abdullah
+            Sajjad
+ */
